724f2fccf3de4b4ad08c3cbad2408e4ad169c3eb
SuryaKiranM <kiransuryam4@gmail.com>
2024-04-01 13:42:16 +0530

    Prime number problem

--- First.cpp
+++ First.cpp
@@ -1,7 +1,27 @@
 #include<iostream>
 
+void PrintPrimeNumbers(){
+    std::cout<<"Prime numbers are ";
+    for(int i=2;i<=10;i++){
+         int count = 0;
+         for(int j=1;j<11;j++){
+             if(i%j==0){
+                 count++;
+             }
+         }
+         if(count>2){
+             continue;
+         }
+         else{
+               std::cout<<i<<" ";
+         }
+    }
+    std::cout<<"\n";
+
+}
 int main(){
 
     std::cout<<"Hello world\n";
+    PrintPrimeNumbers();
     return 0;
 }
